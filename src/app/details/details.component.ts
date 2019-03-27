592711d5e8920585b18850778bf6a2891345e588
Rafael Souza Ramos <rsouzara@everis.com>
2019-03-27 11:58:51 -0300

    details screen

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
+  name:string = "John Lennon"
+  description:string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin hendrerit pellentesque risus, convallis gravida risus hendrerit at. Integer magna sapien, tristique ac consectetur sed, vestibulum iaculis quam. Praesent vestibulum varius posuere. Cras purus orci, tempus vitae finibus sit amet, pharetra non diam. In hac habitasse platea dictumst. Fusce quis mi gravida ligula elementum tincidunt quis nec mi. Mauris vehicula ante eros, sed sodales nisi tincidunt vitae "
+
   constructor() { }
 
   ngOnInit() {
