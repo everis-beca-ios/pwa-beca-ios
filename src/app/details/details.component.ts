ee864c64a14c0e1e858c374631b6d9d2189976a0
Rafael Souza Ramos <rsouzara@everis.com>
2019-03-27 12:38:29 -0300

    get value from url router

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, OnDestroy {
+
+  private urlRoute: any
 
   name:string = "John Lennon"
   description:string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin hendrerit pellentesque risus, convallis gravida risus hendrerit at. Integer magna sapien, tristique ac consectetur sed, vestibulum iaculis quam. Praesent vestibulum varius posuere. Cras purus orci, tempus vitae finibus sit amet, pharetra non diam. In hac habitasse platea dictumst. Fusce quis mi gravida ligula elementum tincidunt quis nec mi. Mauris vehicula ante eros, sed sodales nisi tincidunt vitae "
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.urlRoute = this.route.params.subscribe(params => {
+      console.log(params)
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.urlRoute.unsubscribe();
   }
 
   gotToProfileList() {
