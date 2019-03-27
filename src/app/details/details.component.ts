8884392b3df2181a645d05acb932bfda70cda274
Rafael Souza Ramos <rsouzara@everis.com>
2019-03-27 14:25:44 -0300

    get user details from profile list

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -10,6 +10,8 @@ export class DetailsComponent implements OnInit, OnDestroy {
 
   private urlRoute: any
 
+  private userData: any
+
   name:string = "John Lennon"
   description:string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin hendrerit pellentesque risus, convallis gravida risus hendrerit at. Integer magna sapien, tristique ac consectetur sed, vestibulum iaculis quam. Praesent vestibulum varius posuere. Cras purus orci, tempus vitae finibus sit amet, pharetra non diam. In hac habitasse platea dictumst. Fusce quis mi gravida ligula elementum tincidunt quis nec mi. Mauris vehicula ante eros, sed sodales nisi tincidunt vitae "
 
@@ -17,7 +19,8 @@ export class DetailsComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.urlRoute = this.route.params.subscribe(params => {
-      console.log(params)
+      console.log("params", params)
+      this.userData = params
     })
   }
 
