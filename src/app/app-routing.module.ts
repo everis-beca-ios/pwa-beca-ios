a640c80d3520e8cf9f6a6ff2bad7e8614e6ce221
Felipe Moura Soares <felipe94msoares@gmail.com>
2019-03-28 11:40:38 -0300

    validation login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DetailsComponent } from './details/details.component';  
+import { DetailsComponent } from './details/details.component';
 import { ProfileListComponent } from './profile-list/profile-list.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
-  { path: 'profile-list', component: ProfileListComponent},
-  {path: 'details', component: DetailsComponent},
+  { path: 'profile-list', component: ProfileListComponent },
+  { path: 'details', component: DetailsComponent },
   { path: '', component: LoginComponent, canActivate: [AuthGuard] }
 ];
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,33 +9,45 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  login: FormGroup
-  
+  loginFrom: FormGroup
+  formIsvalid: boolean
+
   constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) {
 
   }
 
 
   ngOnInit() {
-    this.login = this.fb.group({
-      id: ['', Validators.required],
-      passwd: ['', Validators.required]
+    this.loginFrom = this.fb.group({
+      id: ['', Validators.compose([Validators.required, Validators.minLength(5)])],
+      passwd: ['', Validators.compose([Validators.required, Validators.minLength(8)])]
     });
+  }
+
+  get userId() {
+    return this.loginFrom.controls.id.valid || this.loginFrom.controls.id.pristine;
+  }
+  get userPass() {
+    return this.loginFrom.controls.passwd.valid || this.loginFrom.controls.passwd.pristine;
   }
 
   isValid() {
     const formData = new FormData()
-    formData.append('id', this.login.controls.id.value); formData.append('passwd', this.login.controls.passwd.value);
-    this.http.post('https://beca-sn-pwa-instantapps-api.herokuapp.com/login', formData).subscribe(
-      res => {
-        localStorage.setItem('username', res['logado']['name']);
-        localStorage.setItem('isLoggedIn', 'true');
-        this.router.navigate(['profile-list']);
-      },
-      err => {
-        console.error(err);
-      }
-    )
+    if (this.loginFrom.invalid) { this.formIsvalid = false }
+    else {
+      this.formIsvalid = true
+      formData.append('id', this.loginFrom.controls.id.value); formData.append('passwd', this.loginFrom.controls.passwd.value);
+      this.http.post('https://beca-sn-pwa-instantapps-api.herokuapp.com/login', formData).subscribe(
+        res => {
+          if (res == null) { this.formIsvalid = false }
+          else {
+            localStorage.setItem('username', res['logado']['name']);
+            localStorage.setItem('isLoggedIn', 'true');
+            this.router.navigate(['profile-list']);
+          }
+        },
+      )
+    }
   }
 
 }
