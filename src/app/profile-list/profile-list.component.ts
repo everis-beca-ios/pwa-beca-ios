0e10f033a4ef260d35ce8a766f2a54614a9c2aea
cesar.paiva <cesar.giupponi@gmail.com>
2019-03-28 10:03:43 -0300

    Refactoring

--- src/app/profile-list/profile-list.component.ts
+++ src/app/profile-list/profile-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProfileListService } from './profile-list.service';
 
 @Component({
   selector: 'app-profile-list',
@@ -12,10 +12,10 @@ export class ProfileListComponent implements OnInit {
 
   profiles: any;
   name: string;
-  url: string = 'https://beca-sn-pwa-instantapps-api.herokuapp.com/users';
   imageDefault: string = 'https://cdn3.vectorstock.com/i/1000x1000/38/42/hacker-character-avatar-icon-vector-11573842.jpg';
   constructor(
-    private router: Router, private activateRouter: ActivatedRoute, private httclient: HttpClient
+    private _router: Router, 
+    private _profileListService: ProfileListService
   ) {}
 
   ngOnInit() {
@@ -25,24 +25,17 @@ export class ProfileListComponent implements OnInit {
     }
     console.log(this.onlineOffline)
   }
-
-  getProfileData(){
-    this.activateRouter.params.subscribe(params => {
-      this.name = params.name;
-      caches.open('pwa-cache').then(function(cache) {
-        cache.add('https://beca-sn-pwa-instantapps-api.herokuapp.com/users')
-      })
-    });
+  
+  getProfileData() {
+    this.name = this._profileListService.getProfileData();
   }
 
-  getProfileList() {
-    this.httclient.get(this.url).subscribe(response => {
-      this.profiles = response;
-    });
+  async getProfileList() {
+    this.profiles = await this._profileListService.getProfileList();
   }
 
-  showDetails(profile: any){
-    this.router.navigate(['details', profile]);
+  showDetails(profile: any) {
+    this._router.navigate(['details', profile]);
   }
 
 }
