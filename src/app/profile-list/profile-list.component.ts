98f4a2a6418faf85b744d941321b03ec97068d2f
Gabriel Henrique <gabhenrique0@gmail.com>
2019-03-28 10:31:43 -0300

    Add docs inside profile list component

--- src/app/profile-list/profile-list.component.ts
+++ src/app/profile-list/profile-list.component.ts
@@ -1,38 +1,62 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, Input } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
 
+/**
+ * Render a list of users fetching an API that returns an array of objects
+ * @export ProfileListComponent
+ * @class ProfileListComponent
+ * @implements {OnInit}
+ */
 @Component({
-  selector: 'app-profile-list',
-  templateUrl: './profile-list.component.html',
-  styleUrls: ['./profile-list.component.scss']
+	selector: "app-profile-list",
+	templateUrl: "./profile-list.component.html",
+	styleUrls: ["./profile-list.component.scss"]
 })
+
 export class ProfileListComponent implements OnInit {
-  profiles: any;
-  name: string;
-  url: string = 'https://beca-sn-pwa-instantapps-api.herokuapp.com/users';
-  imageDefault: string = 'https://cdn3.vectorstock.com/i/1000x1000/38/42/hacker-character-avatar-icon-vector-11573842.jpg';
-  constructor(
-    private router: Router, private activateRouter: ActivatedRoute, private httclient: HttpClient
-  ) {}
+	profiles: any;
+	name: string;
+	url: string = "https://beca-sn-pwa-instantapps-api.herokuapp.com/users";
+	imageDefault: string = "https://cdn3.vectorstock.com/i/1000x1000/38/42/hacker-character-avatar-icon-vector-11573842.jpg";
 
-  ngOnInit() {
-    this.getProfileData();
-    this.getProfileList();
-  }
+	constructor(
+		private router: Router,
+		private activateRouter: ActivatedRoute,
+		private httclient: HttpClient
+	) {}
 
-  getProfileData(){    
-    this.name = localStorage.getItem('username');
-  }
+	/**
+	 * Init event of ProfileListComponent
+	 * @memberof ProfileListComponent
+	 */
+	ngOnInit() {
+		this.getProfileData();
+		this.getProfileList();
+	}
 
-  getProfileList() {
-    this.httclient.get(this.url).subscribe(response => {
-      this.profiles = response;
-    });
-  }
+	/**
+	 * Get logged profile data inside application from local storage item called username
+	 * @memberof ProfileListComponent
+	 */
+	getProfileData() {
+		this.name = localStorage.getItem("username");
+	}
 
-  showDetails(profile: any){
-    this.router.navigate(['details', profile]);
-  }
+	/**
+	 * Consume profiles API route and fetch inside profiles attribute of this component
+	 * @memberof ProfileListComponent
+	 */
+	getProfileList() {
+		this.httclient.get(this.url).subscribe(response => this.profiles = response);
+	}
 
+	/**
+	 * Attach event to profile card route navigate to details route
+	 * @param {object} profile an object with a single profile details
+	 * @memberof ProfileListComponent
+	 */
+	showDetails(profile: any) {
+		this.router.navigate(["details", profile]);
+	}
 }
